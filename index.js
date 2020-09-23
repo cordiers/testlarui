a58ea324c2c9fce140482fa3ece4867edc3f451e
scordier <sylvain.cordier@sysscale.com>
2020-09-23 17:33:24 +0200

    init project

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
-import express from 'express'
-
-const app = express()
-const port = process.env.PORT || 3000
+var express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({ success: true })
-})
+});
 
 app.get('/version', (req, res) => {
   res.json({ success: true, version: '1.0.0' })
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
